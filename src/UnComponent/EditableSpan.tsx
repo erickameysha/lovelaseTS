d7b1bc4bdf57a20f2b99582e85687c2af977d849
ekameysha <ekameyshaaa@gmail.com>
2023-06-16 19:29:38 +0300

    ласт2

---
+++ src/UnComponent/EditableSpan.tsx
@@ -0,0 +1,28 @@
+import React, {ChangeEvent, useState} from 'react';
+type EditableSpanPropsType = {
+    value: string
+    onChange: (newValue: string) => void
+}
+const EditableSpan = (props: EditableSpanPropsType) => {
+    console.log('EditableSpan called');
+    let [editMode, setEditMode] = useState(false);
+    let [title, setTitle] = useState('rtoot');
+
+    const activateEditMode = () => {
+        setEditMode(true);
+        setTitle(props.value);
+    }
+    const activateViewMode = () => {
+        setEditMode(false);
+        props.onChange(title);
+    }
+    const changeTitle = (e: ChangeEvent<HTMLInputElement>) => {
+        setTitle(e.currentTarget.value)
+    }
+
+    return editMode
+        ? <input value={title} onChange={changeTitle} autoFocus onBlur={activateViewMode}/>
+        : <span onDoubleClick={activateEditMode}>{props.value}</span>
+};
+
+export default EditableSpan;
--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 import {store} from "./state/store";
-import Test from "./Test";
+
 import BasketError from "./commponent/Basket/BasketError";
 import CheckOut from "./commponent/СheckOut/CheckOut";
 
